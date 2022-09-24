202dcf8b53d6841775b6e0269b5ae24d1007a263
Jjjunior7 <herbert18h@gmail.com>
2022-09-24 17:30:00 -0300

    2-Adiciona o exercício 2

--- 01-fundamentos/bloco-04-introducao-a-javascript/dia-1-primeiros-passos/scripts.js
+++ 01-fundamentos/bloco-04-introducao-a-javascript/dia-1-primeiros-passos/scripts.js
@@ -14,3 +14,16 @@ console.log(a-b)
 console.log(a*b)
 console.log(a/b)
 console.log(a%b)
+
+// Utilize if/else para fazer um programa que defina três números em constantes e retorne true se pelo menos uma das três for par. Caso contrário, ele deve retornar false.
+// Bonus: use somente um if.
+
+let num1 = 7;
+let num2 = 13;
+let num3 = 82;
+
+if(num1%2 === 0 || num2%2 === 0 || num3%2 === 0 ){
+    console.log(true)
+} else{
+    console.log(false)
+}
