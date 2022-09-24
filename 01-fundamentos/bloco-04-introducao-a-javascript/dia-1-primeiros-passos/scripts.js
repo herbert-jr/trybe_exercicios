db577864def48b182d09e7f7e2a40a3c76492446
Jjjunior7 <herbert18h@gmail.com>
2022-09-24 18:33:55 -0300

    4 - Adicionei exercício 4

--- 01-fundamentos/bloco-04-introducao-a-javascript/dia-1-primeiros-passos/scripts.js
+++ 01-fundamentos/bloco-04-introducao-a-javascript/dia-1-primeiros-passos/scripts.js
@@ -18,6 +18,8 @@ console.log(a%b)
 // Utilize if/else para fazer um programa que defina três números em constantes e retorne true se pelo menos uma das três for par. Caso contrário, ele deve retornar false.
 // Bonus: use somente um if.
 
+// --------------------------------------------------------------------------------------------
+
 let num1 = 7;
 let num2 = 13;
 let num3 = 89;
@@ -27,6 +29,8 @@ if(num1%2 === 0 || num2%2 === 0 || num3%2 === 0 ){
     even = true
 }
 console.log(even)
+
+// --------------------------------------------------------------------------------------------
 
 // Utilize if/else para fazer um programa que defina três números em constantes e retorne true se pelo menos uma das três for ímpar. Caso contrário, ele deve retornar false.
 // Bonus: use somente um if.
@@ -40,4 +44,29 @@ if(num1%2 !== 0 || num2%2 !== 0 || num3%2 !== 0 ){
     odd = true
 }
 console.log(odd)
+
+// --------------------------------------------------------------------------------------------
+
+// Utilize if/else para escrever um programa que se inicie com dois valores em duas constantes diferentes: o custo de um produto e seu valor de venda. A partir dos valores, calcule quanto de lucro (valor de venda, descontado o custo do produto) a empresa terá ao vender mil desses produtos.
+// Atente que, sobre o custo do produto, incide um imposto de 20%.
+
+// Seu programa também deve emitir uma mensagem de erro e encerrar caso algum dos seus valores de entrada seja menor que zero.
+
+// O lucro de um produto é o resultado da subtração do valor de venda pelo custo do mesmo, sendo que o imposto de 20% também faz parte do valor de custo.
+
+// valorCustoTotal = valorCusto + impostoSobreOCusto;
+// lucro = valorVenda - valorCustoTotal (lucro de um produto);
+
+let custo = 12;
+let venda = 30;
+let valorTotalCusto = custo * 120/100
+let lucro = venda - valorTotalCusto
+
+if( custo < 0 || venda < 0){
+    console.log("Valores Inaválidos")
+} else {
+    console.log("O seu lucro é " + (lucro*1000))
+}
+
+// --------------------------------------------------------------------------------------------
 
