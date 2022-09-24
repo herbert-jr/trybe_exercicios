64ea1f08544351a672275358e7defe587d333d68
Jjjunior7 <herbert18h@gmail.com>
2022-09-24 16:54:15 -0300

    1-Adicionei o primeiro exercício

--- 01-fundamentos/bloco-04-introducao-a-javascript/dia-1-primeiros-passos/scripts.js
+++ 01-fundamentos/bloco-04-introducao-a-javascript/dia-1-primeiros-passos/scripts.js
@@ -1,7 +1,16 @@
-let soma = 0;
+// Faça cinco programas, um para cada operação aritmética básica. Seu programa deve ter duas constantes, a e b, definidas no começo com os valores que serão operados. Faça programas para:
+// Adição 
+// Subtração 
+// Multiplicação 
+// Divisão 
+// Módulo 
+// --------------------------------------------------------------------------------------------
 
-for (let index = 1; index < 50; index++){
-    soma = soma + index
-}
+let a = 6;
+let b = 4;
 
-console.log("A soma total de 1 até 50 é: " + soma)
+console.log(a+b)
+console.log(a-b)
+console.log(a*b)
+console.log(a/b)
+console.log(a%b)
