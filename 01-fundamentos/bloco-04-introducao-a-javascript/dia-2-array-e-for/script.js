2e096f92178e3fbe4e6cde834f47aaba2de92888
Jjjunior7 <herbert18h@gmail.com>
2022-09-25 20:01:05 -0300

    Adicionei exercício 3

--- 01-fundamentos/bloco-04-introducao-a-javascript/dia-2-array-e-for/script.js
+++ 01-fundamentos/bloco-04-introducao-a-javascript/dia-2-array-e-for/script.js
@@ -8,13 +8,19 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 // Some todos os valores contidos no array e imprima o resultado;
 
+// let soma = 0;
+// for(let index = 0; index < numbers.length; index++ ){
+//     soma += numbers[index]
+// }
+// console.log(soma)
+
+// Calcule e imprima a média aritmética dos valores contidos no array;
+
 let soma = 0;
 for(let index = 0; index < numbers.length; index++ ){
     soma += numbers[index]
 }
-console.log(soma)
-
-// Calcule e imprima a média aritmética dos valores contidos no array;
+console.log(soma/numbers.length)
 
 // A média aritmética é o resultado da soma de todos os elementos dividido pelo número total de elementos.
 // Com base no código que acabou de gerar, faça com que, caso o valor final seja maior que 20, imprima a mensagem: “valor maior que 20”. Caso não seja, imprima a mensagem: “valor menor ou igual a 20”;
