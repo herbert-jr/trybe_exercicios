81daaaac96e38044f7e3fcf4c14a67675ca01a69
Jjjunior7 <herbert18h@gmail.com>
2022-09-25 19:57:57 -0300

    Adicionei exercício 2

--- 01-fundamentos/bloco-04-introducao-a-javascript/dia-2-array-e-for/script.js
+++ 01-fundamentos/bloco-04-introducao-a-javascript/dia-2-array-e-for/script.js
@@ -2,11 +2,17 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 // Percorra o array imprimindo todos os valores nele contidos com a função console.log();
 
-for(let index = 0; index < numbers.length; index++ ){
-    console.log(numbers[index])
-}
+// for(let index = 0; index < numbers.length; index++ ){
+//     console.log(numbers[index])
+// }
 
 // Some todos os valores contidos no array e imprima o resultado;
+
+let soma = 0;
+for(let index = 0; index < numbers.length; index++ ){
+    soma += numbers[index]
+}
+console.log(soma)
 
 // Calcule e imprima a média aritmética dos valores contidos no array;
 
