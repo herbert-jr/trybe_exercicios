8b36bd1609e14ca3f54a9fd64f6f73755396364f
Jjjunior7 <herbert18h@gmail.com>
2022-09-25 19:53:17 -0300

    Adicionei exercício 1

--- 01-fundamentos/bloco-04-introducao-a-javascript/dia-2-array-e-for/script.js
+++ 01-fundamentos/bloco-04-introducao-a-javascript/dia-2-array-e-for/script.js
@@ -1,4 +1,10 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
 // Percorra o array imprimindo todos os valores nele contidos com a função console.log();
+
+for(let index = 0; index < numbers.length; index++ ){
+    console.log(numbers[index])
+}
 
 // Some todos os valores contidos no array e imprima o resultado;
 
@@ -16,3 +22,4 @@
 // Utilizando for, crie um array que vá de 1 até 25 e imprima o resultado;
 
 // Utilizando o array que acabou de criar, imprima o resultado da divisão de cada um dos elementos por 2.
+
