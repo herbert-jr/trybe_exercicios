8e1c22b15226bf2ddb608cde0327badee03715f0
Jjjunior7 <herbert18h@gmail.com>
2022-09-25 20:53:00 -0300

    Adicionei exercício 6

--- 01-fundamentos/bloco-04-introducao-a-javascript/dia-2-array-e-for/script.js
+++ 01-fundamentos/bloco-04-introducao-a-javascript/dia-2-array-e-for/script.js
@@ -52,20 +52,29 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 // Descubra quantos valores ímpares existem no array e imprima o resultado. Caso não exista nenhum, imprima a mensagem: “nenhum valor ímpar encontrado”;
 
-let odd = 0;
+// let odd = 0;
+// for(let index = 0; index < numbers.length; index++ ){
+//     if(numbers[index]%2 !== 0){
+//         odd++
+//     }
+// }
+
+// if(odd === 0){
+//     console.log('nenhum valor ímpar encontrado')
+// }else{
+// console.log(odd)
+// }
+
+// Utilizando for, descubra qual o menor valor contido no array e imprima-o;
+
+let smaller = numbers[0]
 for(let index = 0; index < numbers.length; index++ ){
-    if(numbers[index]%2 !== 0){
-        odd++
+    if(smaller>numbers[index]){
+        smaller=numbers[index]
     }
 }
 
-if(odd === 0){
-    console.log('nenhum valor ímpar encontrado')
-}else{
-console.log(odd)
-}
-
-// Utilizando for, descubra qual o menor valor contido no array e imprima-o;
+console.log(smaller)
 
 // Utilizando for, crie um array que vá de 1 até 25 e imprima o resultado;
 
